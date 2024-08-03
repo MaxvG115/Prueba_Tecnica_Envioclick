2441ad7d8eb6b9bbc0554f77d3e8ba3257c1cecc
MaxvG115 <maxivillegas115@gmail.com>
2024-08-03 17:25:06 -0600

    Funcion kmp_search para contar ocurrencias de cada palabra

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -2,7 +2,7 @@
 
 def compute_prefix(word):
     m = len(word)
-    pi = [0] * m # Funcion de prefijos
+    pi = [0] * m # Tabla de prefijos
     k = 0 # Variable para almacenar la longitud del prefijo más largo
 
     # Iterar sobre cada carácter de la palabra, excepto el primero
@@ -17,3 +17,24 @@ def compute_prefix(word):
 
 
 def kmp_search(word, paragraph):
+    n = len(paragraph)
+    m = len(word)
+    pi = compute_prefix(word) # Calculo de tabla de prefijos
+    q = 0 # Progreso de la coincidencia
+    ocurrences = 0
+
+    # Iterar sobre cada carácter del párrafo
+    for i in range(n):
+        while q > 0 and word[q] != paragraph[i]:
+            q = pi[q - 1] # Actualizar q usando el valor previo de pi
+
+        if word[q] == paragraph[i]:
+            q += 1
+
+        # Si q es igual a la longitud de la palabra, se ha encontrado una ocurrencia
+        if q == m:
+            ocurrences += 1 # Incrementar el contador de ocurrencias
+            q = pi[q - 1]
+    
+    return ocurrences
+
